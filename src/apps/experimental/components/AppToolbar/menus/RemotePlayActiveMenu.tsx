HazardousPeach/jellyfin-web

commit 588e9e38f7c165c048e573156356f3fdd1419d5a

Don't send unsupported commands to Cast receiver

src/plugins/chromecastPlayer/plugin.js
@@ -678,11 +678,7 @@ class ChromecastPlayer {
                 'SetAudioStreamIndex',
                 'SetSubtitleStreamIndex',
                 'DisplayContent',
-                'SetRepeatMode',
-                'SetShuffleQueue',
-                'EndSession',
-                'PlayMediaSource',
-                'PlayTrailers'
+                'SetRepeatMode'
             ]
         };
     }
@@ -890,13 +886,7 @@ class ChromecastPlayer {
     }
 
     playTrailers(item) {
-        this._castPlayer.sendMessage({
-            options: {
-                ItemId: item.Id,
-                ServerId: item.ServerId
-            },
-            command: 'PlayTrailers'
-        });
+        console.warn('[chromecastPlayer] Playing trailers is not supported.');
     }
 
     setRepeatMode(mode) {
@@ -909,12 +899,7 @@ class ChromecastPlayer {
     }
 
     setQueueShuffleMode(value) {
-        this._castPlayer.sendMessage({
-            options: {
-                ShuffleMode: value
-            },
-            command: 'SetShuffleQueue'
-        });
+        console.warn('[chromecastPlayer] Setting shuffle queue mode is not supported.');
     }
 
     toggleMute() {

commit 669eb18637d1865351a04e6c218f41bf325387f8

Fix dialog box not appearing for chromecast disconnect
Fix a bug introduced in 588e9e38f7 preventing the chromecast from being
detected as supporting ending a session, causing the dialog box about
whether you want to end the session or not to not appear.

**Background**

As far as I can tell, there are two supported ways for Jellyfin to end a
session with a PlauerPlugin: sending an "EndSession" command, or calling
endSession on the chromecast SDK. The code used to check if the
PlayerPlugin listed "EndSession" as a supported command, and if so,
called the endSession method. But, the chromecast doesn't actually
support "EndSession" as a message, so someone removed it from the list
of supported messages, even though its the only PlayerPlugin that has
the endSession method that is actually called.

I think this logic also would have failed if a remote session player
reported EndSession as a supported command, since the remote session
player plugin doesn't have an endSession method.

**Changes**

I added a type annotation into plugin manager that would surface some of
the duck-typed weirdness leading to these edge cases. Instead of calling
endSession when the "EndSession" **message** is supported, I just call
sendMessage with that message. Then, I changed the chromecast plugin to
report the EndSession message as supported, but added another case in
sendMessage that would call endSession on anything that had it on the
EndSession command. This should fix both the chromecast issue and any
weirdness with other remote sessions that did support EndSession as a
message (not a method).

The type annotation surfaced one other bug, that this.player was typed
as PlayerPlugin | undefined, but it was copied from a place that was
actually PlayerPlugin | null, so I fixed that there too, hope that's the
right thing for this maintainer community.

**Other Info**

This change combines well with #7587, which prevents a runtime error

src/apps/experimental/components/AppToolbar/menus/RemotePlayActiveMenu.tsx
@@ -67,7 +67,9 @@ const RemotePlayActiveMenu: FC<RemotePlayActiveMenuProps> = ({
                 onMenuClose();
 
                 if (id === 'yes') {
-                    playbackManager.getCurrentPlayer().endSession();
+                    playbackManager.sendCommand(
+                        { Name: 'EndSession' },
+                        playbackManager.getCurrentPlayer());
                 }
                 playbackManager.setDefaultPlayerActive();
             }).catch(() => {

src/apps/stable/features/playback/utils/playbackSubscriber.ts
@@ -42,7 +42,7 @@ export interface PlaybackSubscriber {
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export abstract class PlaybackSubscriber {
-    protected player: PlayerPlugin | undefined;
+    protected player: PlayerPlugin | null = null;
 
     private readonly playbackManagerEvents = {
         [PlaybackManagerEvent.PlaybackCancelled]: this.onPlaybackCancelled?.bind(this),

src/components/playback/playbackmanager.js
@@ -3736,6 +3736,7 @@ export class PlaybackManager {
         this._skipSegment = bindSkipSegment(self);
     }
 
+    /** @returns {import('types/plugin').PlayerPlugin | null} */
     getCurrentPlayer() {
         return this._currentPlayer;
     }
@@ -4287,6 +4288,12 @@ export class PlaybackManager {
             case 'ToggleFullscreen':
                 this.toggleFullscreen(player);
                 break;
+            case 'EndSession':
+                if ('endSession' in player && typeof player.endSession === 'function') {
+                    player.endSession();
+                    break;
+                }
+                // falls through
             default:
                 if (player.sendCommand) {
                     player.sendCommand(cmd);

src/components/playback/playerSelectionMenu.js
@@ -149,7 +149,7 @@ function disconnectFromPlayer(currentDeviceName) {
         }).then(function (id) {
             switch (id) {
                 case 'yes':
-                    playbackManager.getCurrentPlayer().endSession();
+                    playbackManager.sendCommand('EndSession');
                     playbackManager.setDefaultPlayerActive();
                     break;
                 case 'no':

src/plugins/chromecastPlayer/plugin.js
@@ -678,7 +678,8 @@ class ChromecastPlayer {
                 'SetAudioStreamIndex',
                 'SetSubtitleStreamIndex',
                 'DisplayContent',
-                'SetRepeatMode'
+                'SetRepeatMode',
+                'EndSession'
             ]
         };
     }